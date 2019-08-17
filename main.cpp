ef259fabd28985941c60cba86bd91599fe065015
jasmineagrawal <53893949+jasmineagrawal@users.noreply.github.com>
2019-08-17 10:49:30 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,40 @@
-#include <stdio.h>
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
-int sqrt(int x)
-{
-	if(x==0|| x==1)
-		return x;
-int start=0,end=x,val;
-	
-		while(start<=end)
-		{
-			int mid=(start+end)/2;
-			if(mid*mid==x)
-			{val=mid;
-			return val;
-			}
-			if(mid*mid<x)
-			{
-				start=mid+1;
-				val=mid;
-			}
-			else
-				end=mid-1;
-		}
-		return val;
-}
 int main()
 {
-	int n;
-	cout<<"enter the  number";
-	cin>>n;
-	int a=sqrt(n);
-	cout<<"value="<<a<<endl;
-	return 0;
+	int t,n,k,a[20],flag=0;
+	ifstream inFile;
+	inFile.open("input.txt");
+	if(!inFile)
+	{
+		cout<<"unable";
+		
+	}
+	inFile>>t;
+	cout<<"no.of test cases:"<<t<<endl;
+	for(int i=0;i<t;i++)
+	{
+		flag=0;
+		inFile>>n;
+		cout<<"no. of test cases"<<n<<endl;
+	
+	inFile>>k;
+	cout<<"the key"<<k<<endl;
+	cout<<"the element"<<endl;
+	for( int i=0;i<=n;i++)
+	{
+		inFile >>a[i];
+		cout<<a[i]<<" ,";
+		if(a[i]==k)
+			flag=1;
+	}
+	if(flag==1)
+		cout<<endl<<1<<"found";
+	else
+		cout<<endl<<-1<<"not found"<<endl;
+}
+inFile.close();
+return 0;
 }
