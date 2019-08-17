fcc7efd728cc201f64ee9684f2bb7766c82bb668
jasmineagrawal <53893949+jasmineagrawal@users.noreply.github.com>
2019-08-17 10:31:46 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,39 +1,37 @@
-#include <fstream>
+#include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char **argv)
+int sqrt(int x)
 {
-	int t,n,k,a[20],flag=0;
-	ifstream inFile;
-	inFile.open("input.txt");
-	if(inFile)
-	{
-		cout<<"unable to open file";
-	}
-	inFile >> t;
-	cout<<"no. of test cases:"<<t<<endl;
-	for(int i=0;i<t;i++)
-	{
-		flag=0;
-		inFile>>n;
-		cout<<"the no. of elements: "<<n<<endl;
-		inFile>>k;
-		cout<<"the key"<<k<<endl;
-		cout<<"the lements: "<<endl;
-		for(i=0;i<=n;i++)
+	if(x==0|| x==1)
+		return x;
+int start=0,end=x,val;
+	
+		while(start<=end)
 		{
-			inFile >>a[i];
-			cout<<a[i]<<" , ";
-			if(a[i]==k)
-				flag=1;
-		}
-		if(flag==1)
-			cout<<endl<<1<<"key found";
+			int mid=(start+end)/2;
+			if(mid*mid==x)
+			{val=mid;
+			return val;
+			}
+			if(mid*mid<x)
+			{
+				start=mid+1;
+				val=mid;
+			}
 			else
-				cout<<endl<<-1<<"key not found"<<endl;
-	}
-inFile.close();
-return 0;
+				end=mid-1;
+		}
+		return val;
+}
+int main()
+{
+	int n;
+	cout<<"enter the  number";
+	cin>>n;
+	int a=sqrt(n);
+	cout<<"value="<<a<<endl;
+	return 0;
 }
