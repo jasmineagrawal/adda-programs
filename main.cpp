5566d3d099a597ac849193987fabecdd71c4d114
jasmineagrawal <53893949+jasmineagrawal@users.noreply.github.com>
2019-08-24 10:45:34 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,40 +1,36 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
-	int t,n,k,a[20],flag=0;
-	ifstream inFile;
-	inFile.open("input.txt");
-	if(!inFile)
+	int arr[20],n,temp;
+	printf("enter the number of elements in an array");
+	scanf("%d",&n);
+	printf("enter the elements of the array");
+	for(int i=0;i<n;i++)
 	{
-		cout<<"unable";
+	scanf("%d",&arr[i]);
+	}
+	int k;
+	printf("enter the k largest elements to be printed");
+	scanf("%d",&k);
+	
+	for(int i=0;i<n-1;i++)
+	{
+		
+		for(int j=1;j<n-1-i;j++)
+		{
+			if(arr[j+1]<arr[j])
+			{
+			temp=arr[j];
+			arr[j]=arr[j+1];
+			arr[j+1]=temp;
+		}
 		
 	}
-	inFile>>t;
-	cout<<"no.of test cases:"<<t<<endl;
-	for(int i=0;i<t;i++)
-	{
-		flag=0;
-		inFile>>n;
-		cout<<"no. of test cases"<<n<<endl;
-	
-	inFile>>k;
-	cout<<"the key"<<k<<endl;
-	cout<<"the element"<<endl;
-	for( int i=0;i<=n;i++)
-	{
-		inFile >>a[i];
-		cout<<a[i]<<" ,";
-		if(a[i]==k)
-			flag=1;
+for(int i=n-1;i>n-1-k;i--)
+{
+	printf("the largest no. is: %d",arr[i]);
 	}
-	if(flag==1)
-		cout<<endl<<1<<"found";
-	else
-		cout<<endl<<-1<<"not found"<<endl;
+	return 0;
 }
-inFile.close();
-return 0;
 }
